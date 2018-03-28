708bf1c2b3d424cf9e10a359f7487facc03a3f1b
Alexandru Savin <savin.alexandru@live.com>
2018-03-28 17:38:01 +0200

    feat: draw all murders on map

--- ass2/js/part2.js
+++ ass2/js/part2.js
@@ -1,25 +1,52 @@
-const width = 650
-const height = 600
+const width = 800
+const height = 700
+
+const boroughColors = {
+    "StatenIsland": "#edf8fb",
+    "Queens": "#b3cde3",
+    "Bronx": "#8c96c6",
+    "Manhattan": "#8856a7",
+    "Brooklyn": "#810f7c"
+};
 
 const loadChart = (geoJson) => {
 
     const projection = d3.geoMercator()
-                        .fitExtent([[0, 0], [width, height]], geoJson);
-
+        .fitExtent([[0, 0], [width, height]], geoJson);
     const geoGenerator = d3.geoPath(projection);
 
+    //create svg
     const svg = d3.select('#map')
-                .append('svg')
-                .attr('width', width)
-                .attr('height',height)
-                .attr('id','Figure1');
-
-    const u = svg.selectAll('path')
-                .data(geoJson.features);
-    
-    u.enter()
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('id', 'Figure1');
+    // plot paths
+    svg.selectAll('path')
+        .data(geoJson.features)
+        .enter()
         .append('path')
-        .attr('d', geoGenerator);
+        .attr('d', geoGenerator)
+        .style('fill', (d) => {
+            return boroughColors[d.properties.BoroName]
+        });
+
+    //add points
+    d3.csv("data/all_murder.csv", (data) => {
+        svg.selectAll('circle')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('cx', (d) => {
+                return projection([d.lon, d.lat])[0];
+            })
+            .attr('cy', (d) => {
+                return projection([d.lon, d.lat])[1];
+            })
+            .attr('r', 2)
+            .style('fill', 'green')
+            .attr('id', "x" + d.lon + d.lat)
+    })
 };
 
 window.onload = () => {
