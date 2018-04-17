e628b3cd81f6206d241e6ce11ea2cc9bb3fde87f
Jesper V. Mark <jespervmark@gmail.com>
2018-04-17 14:55:19 +0200

    Data load example

--- final-project/js/visualization.js
+++ final-project/js/visualization.js
@@ -1,11 +1,24 @@
 var Visualization = (function(){ 
 
-    var importData = function(){
+    var loadDataAndSetupVisualizations = function(){
+        d3.csv("/data/placeholder.csv", function(data){
+            setupMap();
+            setupDoughnut()
+            setupCalendar();
+            setupScatterPlot();
+            setupTreemap();
+        })
 
+        /* we can make different csv imports for each viz depending on the preprocessing e.g.
+        
+        d3.csv("/data/mapData.csv", function(data){
+            setupMap(data);
+        })
+        */
     };
 
     var setupMap = function(){
-        console.log(d3.version);
+        console.log(d3.version);        
         console.log("init map");
     };
 
@@ -26,11 +39,7 @@ var Visualization = (function(){
     };
 
     var load = function(){
-        setupMap();
-        setupDoughnut()
-        setupCalendar();
-        setupScatterPlot();
-        setupTreemap();
+        loadDataAndSetupVisualizations();
     }
 
     return {
