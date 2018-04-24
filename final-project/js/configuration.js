d3a63c01de6a54867ea41107b1e425a31f86483a
Jesper V. Mark <jespervmark@gmail.com>
2018-04-24 19:23:48 +0200

    Divisions colored by number of arrests

---
+++ final-project/js/configuration.js
@@ -0,0 +1,45 @@
+var config = {
+
+    colors : {
+        hover : 'rgba(47, 93, 102, 0.5)',
+        click : 'rgba(47, 93, 102, 1)',
+        basemap : [
+            '#D8E5E5',
+            '#90D1D1',
+            '#B1EDED',
+            '#63B5B5',
+            '#E2F1F1',
+            '#A0F4F4',
+            '#7BCECE',
+            '#CAC6D1',
+            '#EBE7F3',
+            '#C4C4C5',
+            '#F0E9FF',
+            '#BCB6C6',
+            '#E3E0EA',
+            '#DBD5E6',
+            '#D5CDE4',
+            '#9C99A1',
+            '#B1A5CB',
+            '#C8B8E9',
+            '#DFD4F3',
+            '#9F9CA4',
+            '#ABA6B5'
+            ],
+        scaleColors : [
+            '#D8E5E5',
+            '#D8D3D5',
+            '#D8C5CA',
+            '#D8BEC3',
+            '#D8B1B5',
+            '#D89CAA',
+            '#D88A95',
+            '#D87C8A',
+            '#D86777',
+            '#D8555A',
+            '#D8404E'
+        ]
+
+    }
+
+}
--- final-project/js/visualization.js
+++ final-project/js/visualization.js
@@ -1,60 +1,259 @@
 const Visualization = (() => {
 
     var loadDataAndSetupVisualizations = function () {
-
-        /*d3.csv('./data/all-data.csv', (data) => {
-            setupTimeline(data);
-            hideLoader('timeline');
-        });*/
-
-        d3.csv('/data/placeholder.csv', function (data) {
-            setupMap();
-            //setupTimeline(data);
-            setupDoughnut()
-            setupCalendar();
-            setupScatterPlot();
-            setupTreemap();
-        })
-
-        /* we can make different csv imports for each viz depending on the preprocessing e.g.
-        
-        d3.csv('/data/mapData.csv', function(data){
-            setupMap(data);
-        })
-        */
+        setupMap();
+        setupTimeline();
+        setupDoughnut()
+        setupCalendar();
+        setupScatterPlot();
+        setupTreemap();
     };
 
+    /* --------- MAP AND TIMELINE ----------  */
+
     var setupMap = function () {
+        const width = 600;
+        const height = 800;
+        margin = { top: 40, right: 50, bottom: 40, left: 50 };
+
+        var isAnimationToggled = false;
+        var dataset;
+        var xScale, yScale;
+        var brush;
+        const map = {
+            width: width,
+            height: height * 8 / 10,
+            timeline: {
+                width: width - margin.left - margin.right,
+                height: height * 3 / 10 - margin.top - margin.bottom,
+                padding: 30,
+            }
+        };
+
+        const drawMap = (geoJson) => {
+
+            const projection = d3.geoMercator()
+                .fitExtent([[0, 0], [map.width, map.height]], geoJson);
+            const geoGenerator = d3.geoPath(projection);
+        
+            //create svg
+            const svg = d3.select('#map')
+                .append('svg')
+                .attr('width', map.width)
+                .attr('height', map.height)
+                .attr('id', 'Figure1');
+        
+            // plot paths
+            svg.selectAll('path')
+                .data(geoJson.features)
+                .enter()
+                .append('path')
+                .attr('d', geoGenerator);
+                /*.on("mouseover", function(d, i) {
+                    d3.select(this).style('fill', config.colors.hover);
+                })
+                .on("mouseout", function(d, i) {
+                    d3.select(this).style('fill', config.colors.basemap[i]);
+                })*/
+
+                d3.json('./data/district-agr.json', (err, data) => {
+                                    
+                    var min = d3.min(data.Districts, (d) => {
+                        return d.count;
+                    });
+                    var max = d3.max(data.Districts, (d) => {
+                        return d.count;
+                    });
+                
+                    var colorScale = d3.scaleQuantize()
+                        .domain([min, max])
+                        .range(config.colors.scaleColors);
+
+                    svg.selectAll('path')
+                        .style("fill", (d, i) => {
+                            console.log(data.Districts[i].count);
+                            console.log(colorScale(data.Districts[i].count));
+                            
+                            return colorScale(data.Districts[i].count);
+                        });
+
+                });
+        
+            const radialGradient = svg.append("defs")
+                .append("radialGradient")
+                .attr("id", "radial-gradient");
+        
+            radialGradient.append("stop")
+                .attr("offset", "0%")
+                .attr("stop-color", "red")
+                .attr("stop-opacity", 0.5);
+        
+            radialGradient.append("stop")
+                .attr("offset", "100%")
+                .attr("stop-color", "rgba(255, 0, 0, 0)")
+                .attr("stop-opacity", 0);
+        
+            //create title 
+            svg.append("text")
+                .attr("x", (width / 2))             
+                .attr("y", map.height)
+                .attr("text-anchor", "middle")   
+                .text("LAPD Divisons");
+                
+            // add labels to divisions
+            svg.selectAll('text.divison-label')
+                .data(geoJson.features)
+                .enter()
+                .append('text')
+                .attr('class', 'divison-label')
+                .attr("x", function(d) {
+                    return geoGenerator.centroid(d)[0];
+                })
+                .attr("y", function(d) {
+                    return geoGenerator.centroid(d)[1];
+                })
+                .text(function(d) {
+                    return d.properties.name;  
+                });	
+        
+            /*//add points
+            d3.json('./data/agr.json', (data) => {
+                //console.log(data);
+        
+                svg.selectAll('circle')
+                    .data(data)
+                    .enter()
+                    .append('circle')
+                    .attr('cx', (d) => {
+                        //console.log(d);
+        
+                        return projection([d.location.y, d.location.x])[0];
+                    })
+                    .attr('cy', (d) => {
+                        return projection([d.location.y, d.location.x])[1];
+                    })
+                    .attr('r', (d) => {
+                        return d.count / 1000
+                    })
+                    // .style('fill', 'rgba(255, 0, 0, 0.2)')
+                    .style('fill', 'url(#radial-gradient)')
+                    .attr('id', (d) => { return d.lat + '|' + d.lon })
+            })*/
+        };
+
         d3.json('./data/la.geojson', (err, geoJson) => {
             drawMap(geoJson);
             hideLoader('map');
         });
     };
 
+    const setupTimeline = () => {
+
+        const drawTimeline = (data) => {
+            const entries = d3.nest()
+                .key(function (d) { return d["Arrest Date"]; })
+                .entries(data);
+        
+            const parseTime = d3.timeParse('%m/%d/%Y');
+        
+            for (let i = 0; i < data.length; i++) {
+                data[i]["Arrest Date"] = parseTime(data[i]["Arrest Date"]);
+            }
+        
+            data.sort((x, y) => {
+                return d3.ascending(x["Arrest Date"], y["Arrest Date"]);
+            });
+        
+            const startDate = d3.min(entries, function (d) {
+                return parseTime(d.key);
+            });
+            const endDate = d3.max(entries, function (d) {
+                return parseTime(d.key);
+            });
+        
+            const xScale = d3.scaleTime()
+                .domain([startDate, endDate])
+                .rangeRound([0, map.timeline.width]);
+        
+            const yScale = d3.scaleLinear()
+                .domain([0, d3.max(entries, function (d) { return d.values.length; })])
+                .range([map.timeline.height, 0]);
+        
+            const xAxis = d3.axisBottom()
+                .scale(xScale);
+        
+            const yAxis = d3.axisLeft()
+                .scale(yScale)
+                .ticks(10); // check how many
+        
+            const svg = d3.select('#timeline')
+                .append('svg')
+                .attr('width', map.timeline.width + margin.left + margin.right)
+                .attr('height', map.timeline.height + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        
+            svg.selectAll('rect')
+                .data(entries)
+                .enter()
+                .append('rect')
+                .attr('x', function (d) {
+                    return xScale(parseTime(d.key));
+                })
+                .attr('y', function (d) {
+                    return yScale(d.values.length);
+                })
+                .attr('width', map.timeline.width / entries.length)
+                .attr('height', function (d) {
+                    return map.timeline.height - yScale(d.values.length);
+                })
+                .attr('fill', 'darkslateblue');
+        
+            svg.append('g')
+                .attr('class', 'axis')
+                .attr('transform', 'translate(0,' + (map.timeline.height) + ')')
+                .call(xAxis);
+        
+            svg.append('g')
+                .attr('class', 'axis')
+                .attr('transform', 'translate(0,0)')
+                .call(yAxis);
+        
+            svg.append('text')
+                .attr('class', 'label')
+                .attr('text-anchor', 'middle')
+                .attr('x', map.timeline.width / 2)
+                .attr('y', map.timeline.height + map.timeline.padding)
+                .text('Day');
+        
+            svg.append('text')
+                .attr('text-anchor', 'middle')
+                .attr('transform', 'translate(' + -(map.timeline.padding) + ',' + (map.timeline.height / 2) + ')rotate(-90)')
+                .text('# of Arrests');
+        
+            const x = d3.scaleTime()
+                .range([0, map.timeline.width]);
+        };
+
+        d3.json('./data/all-arrests.json', (err, data) => {
+            //drawTimeline(data);
+            hideLoader('timeline');
+        });
+    };
+
+
+    /* --------- DOUGHNUT ----------  */
+
     var setupDoughnut = function () {
         console.log('init doughnut');
     };
+
+    /* --------- CALENDAR ----------  */
 
     var setupCalendar = function () {
         drawCalendar();
         hideLoader();
     };
-
-    var setupScatterPlot = function () {
-        console.log('init scatter plot');
-    };
-
-    var setupTreemap = function () {
-        console.log('init treemap');
-    };
-
-    var hideLoader = function (parentId) {
-        $('#' + parentId).find('.loader').hide()
-    };
-
-    var load = function () {
-        loadDataAndSetupVisualizations();
-    }
 
     var drawCalendar = function(data){
         var width = 960,
@@ -114,6 +313,28 @@ const Visualization = (() => {
                 + "H" + (w0 + 1) * cellSize + "Z";
         }
     }
+
+    /* --------- SCATTER PLOT ----------  */
+
+    var setupScatterPlot = function () {
+        console.log('init scatter plot');
+    };
+
+    /* --------- TREE MAP ----------  */
+
+    var setupTreemap = function () {
+        console.log('init treemap');
+    };
+
+    var hideLoader = function (parentId) {
+        $('#' + parentId).find('.loader').hide()
+    };
+
+    var load = function () {
+        loadDataAndSetupVisualizations();
+    };
+
+    
 
     return {
         load: load
